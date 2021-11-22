973452a84894d76914dae790df03f59a854736ce
Matt Keys <MatthewjKeys22@gmail.com>
2021-11-22 16:21:30 -0700

    its broken and i dont care

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+
 //Porting
 const PORT = process.env.PORT || 3001;
 //App Instance
@@ -6,21 +9,39 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//Database query
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
 
 
-// Default response for any other request (Not Found)
+
+
+
+
+
+
+// Catchall
 app.use((req, res) => {
     res.status(404).end();
   });
 
-  
+
 //Listener 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
